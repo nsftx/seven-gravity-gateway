faeeeaf3d6a9b97636270962ac6dacab7fee7280
Pandzic Ivan <pandzic.ivan@nsoft.ba>
2016-10-05 09:07:38 +0200

    Make gateway-singleton
    ref T18715

--- src/platform_gateway.js
+++ src/platform_gateway.js
@@ -1,34 +1,52 @@
 var platform = require('./messaging/platform'),
     PubSub = require('./pub_sub');
 
-function PlatformGateway() {
-    this.pubSub = new PubSub();
-    this.groupId = 'default';
-    this.msgOrigin = 'platform';
+// Gateway must be singleton
+var PlatformGateway = (function() {
 
-    window.addEventListener('message', function (data) {
-        if(data.origin != this.msgOrigin) {
-            this.pubSub.publish(data);
+    var instantiated = false;
+
+    function Gateway() {
+        this.pubSub = new PubSub();
+        this.groupId = 'default';
+        this.msgOrigin = 'platform';
+
+        window.addEventListener('message', function (data) {
+            if(data.origin != this.msgOrigin) {
+                this.pubSub.publish(data);
+            }
+        }.bind(this));
+    }
+
+    Gateway.prototype.subscribe = function(data) {
+        this.pubSub.subscribe(data);
+    };
+
+    Gateway.prototype.unsubscribe = function(data) {
+        this.pubSub.unsubscribe(data);
+    };
+
+    Gateway.prototype.clearSubscriptions = function() {
+        this.pubSub.clearSubscriptions();
+    };
+
+    Gateway.prototype.sendMessage = function(productFrame, data, origin) {
+        //Attach sender origin to data
+        data.origin = this.msgOrigin;
+        platform.sendMessage(productFrame, data, origin);
+    };
+
+    return {
+        getInstance : function() {
+            if(!instantiated) {
+                instantiated= true;
+                return new Gateway();
+            } else {
+                return false;
+            }
         }
-    }.bind(this));
-}
+    }
 
-PlatformGateway.prototype.subscribe = function(data) {
-    this.pubSub.subscribe(data);
-};
-
-PlatformGateway.prototype.unsubscribe = function(data) {
-    this.pubSub.unsubscribe(data);
-};
-
-PlatformGateway.prototype.clearSubscriptions = function() {
-    this.pubSub.clearSubscriptions();
-};
-
-PlatformGateway.prototype.sendMessage = function(productFrame, data, origin) {
-    //Attach sender origin to data
-    data.origin = this.msgOrigin;
-    platform.sendMessage(productFrame, data, origin);
-};
+})();
 
 module.exports = PlatformGateway;
--- src/product_gateway.js
+++ src/product_gateway.js
@@ -1,36 +1,53 @@
 var product = require('./messaging/product'),
     PubSub = require('./pub_sub');
 
-function ProductGateway(groupId) {
-    this.pubSub = new PubSub(groupId);
-    this.groupId = groupId;
-    this.msgOrigin = 'product';
-    
-    window.addEventListener('message', function (data) {
-        if(data.groupId == this.groupId && data.origin != this.msgOrigin) {
-            this.pubSub.publish(data);
+// Gateway must be singleton
+var ProductGateway = (function(){
+
+    var instances = {};
+
+    function Gateway(groupId) {
+        this.pubSub = new PubSub(groupId);
+        this.groupId = groupId;
+        this.msgOrigin = 'product';
+
+        window.addEventListener('message', function (data) {
+            if(data.groupId == this.groupId && data.origin != this.msgOrigin) {
+                this.pubSub.publish(data);
+            }
+        }.bind(this));
+    }
+
+    Gateway.prototype.subscribe = function(data) {
+        this.pubSub.subscribe(data);
+    };
+
+    Gateway.prototype.unsubscribe = function(data) {
+        this.pubSub.unsubscribe(data);
+    };
+
+    Gateway.prototype.clearSubscriptions = function() {
+        this.pubSub.clearSubscriptions();
+    };
+
+    Gateway.prototype.sendMessage = function(data, origin) {
+        //Attach origin and id of group
+        data.origin = this.msgOrigin;
+        data.groupId = this.groupId;
+
+        product.sendMessage(data, origin);
+    };
+
+    return {
+        getInstance : function(groupId) {
+            if(!instances[groupId] && groupId) {
+                instances[groupId] = true;
+                return new Gateway(groupId);
+            } else {
+                return false;
+            }
         }
-    }.bind(this));
-}
-
-ProductGateway.prototype.subscribe = function(data) {
-    this.pubSub.subscribe(data);
-};
-
-ProductGateway.prototype.unsubscribe = function(data) {
-    this.pubSub.unsubscribe(data);
-};
-
-ProductGateway.prototype.clearSubscriptions = function() {
-    this.pubSub.clearSubscriptions();
-};
-
-ProductGateway.prototype.sendMessage = function(data, origin) {
-    //Attach origin and id of group
-    data.origin = this.msgOrigin;
-    data.groupId = this.groupId;
-
-    product.sendMessage(data, origin);
-};
+    }
+})();
 
 module.exports = ProductGateway;
