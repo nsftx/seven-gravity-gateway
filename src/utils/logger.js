d53b1321640f162912b7b6e142c5bc8085e1a398
Pandzic Ivan <pandzic.ivan@nsoft.ba>
2016-10-26 11:44:23 +0200

    Create debug mode for gateway
    ref T20507

---
+++ src/utils/logger.js
@@ -0,0 +1,20 @@
+var logger = {
+    debugMode : false, //Verbosity setting
+
+    output : function() {
+        if(this.debugMode) {
+            //Convert to array
+            var args = Array.prototype.slice.call(arguments);
+            //First argument is notification type (log, error, warn, info)
+            var type = args.splice(0,1);
+
+            if(console[type]) {
+                console[type](args);
+            } else {
+                console.log(args);
+            }
+        }
+    }
+};
+
+module.export = logger;
