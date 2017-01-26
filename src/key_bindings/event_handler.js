4a69db202b2daaf9322c46e241d1cce6327392ec
Pandzic Ivan <pandzic.ivan@nsoft.ba>
2017-01-26 17:09:49 +0100

    Handle combined events
    ref T24142

--- src/key_bindings/event_handler.js
+++ src/key_bindings/event_handler.js
@@ -1,6 +1,12 @@
 var logger = require('../utils/utils').logger;
 
 function KeyBindings(config, eventCb, eventName) {
+    this.MODIFIER_KEYS = {
+        shiftKey : ['Shift', 16],
+        ctrlKey : ['Ctrl', 17],
+        altKey : ['Alt', 18, 'AltGr', 225],
+        metaKey : ['Meta', 91]
+    };
     this.config = config;
     this.eventCallback = eventCb;
     this.eventName = eventName;
@@ -20,11 +26,45 @@ KeyBindings.prototype = {
             eventKey = e.key || e.keyIdentifier,
             eventList = this.config[e.type];
 
-        if(!eventList || eventList.indexOf(eventCode) === -1 && eventList.indexOf(eventKey) === -1) {
-            logger.out('info', 'Key ' + eventKey + ' is not marked for propagation.');
+        if (!eventList) {
+            logger.out('info', 'Event ' + e.type + ' is not marked for propagation.');
             return false;
         }
 
+        for (var i = 0; i < eventList.length; i++) {
+            var keyBinding = eventList[i];
+            // Check if key is listed for propagation
+            if (keyBinding.indexOf(eventCode) !==-1 || keyBinding.indexOf(eventKey) !==-1) {
+                // Check if key is combined with modifier (Alt, Ctrl...)
+                if(keyBinding.indexOf('+') !== -1) {
+                    var modifier = this.getModifierKey(keyBinding);
+                    if(modifier && e[modifier] === true) {
+                        this.triggerEventCallback(e);
+                        return;
+                    }
+                } else {
+                    this.triggerEventCallback(e);
+                    return;
+                }
+            }
+        }
+
+        logger.out('info', 'Key ' + eventKey + ' is not marked for propagation.');
+    },
+
+    getModifierKey : function(keyBinding) {
+        for(var key in this.MODIFIER_KEYS) {
+            var modifierList = this.MODIFIER_KEYS[key];
+            for(var i=0; i<modifierList.length; i++) {
+                if(keyBinding.indexOf(modifierList[i]) !== -1) {
+                    return key;
+                }
+            }
+
+        }
+    },
+
+    triggerEventCallback : function(e) {
         var data = {
             action : this.eventName,
             event : e.type,
