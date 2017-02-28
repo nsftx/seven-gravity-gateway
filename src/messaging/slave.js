c422267321ac0fc4d55588f61f181f3e1dce173e
Jadranko Dragoje <dragoje.jadranko@nsoft.ba>
2017-02-28 09:47:03 +0100

    Add worker message proxy
    Ref T26174

--- src/messaging/slave.js
+++ src/messaging/slave.js
@@ -1,20 +1,27 @@
-function Porthole(){
+function Porthole() {
     this.worker = null;
     this.msgBlacklist = null;
 }
 
 Porthole.prototype = {
-
-    setWorker: function(worker, msgBlacklist) {
+    setWorker: function (worker, msgBlacklist) {
         this.worker = worker;
         this.msgBlacklist = msgBlacklist;
+        this.setWorkerProxy();
     },
-
-    sendMessage: function(data, domain) {
+    setWorkerProxy: function () {
+      if (this.worker) {
+          // Listen message from worker and proxy same message to slave
+          this.worker.addEventListener('message', function (event) {
+              window.postMessage(event.data, window.location.origin);
+          });
+      }
+    },
+    sendMessage: function (data, domain) {
         var windowDomain = domain || '*';
         window.parent.postMessage(data, windowDomain);
 
-        if(this.worker && this.msgBlacklist.indexOf(data.action) === -1) {
+        if (this.worker && this.msgBlacklist.indexOf(data.action) === -1) {
             this.worker.postMessage(data);
         }
     }
--- test/slave_mesagging.js
+++ test/slave_mesagging.js
@@ -2,20 +2,18 @@ var porthole = require('../src/messaging/slave'),
     assert = require('assert'),
     dom = require('jsdom-global')();
 
-describe('Posting messages to platform.', function() {
-    it('Received value should be 3.14', function(done) {
-        window.addEventListener('message', function(e) {
-            if(e.data != '3.14') {
-                setTimeout(function() {
-                    done(new Error("Value is " + e.data + ' but expected to be 3.14'));
-                });
+describe('Posting messages to platform.', function () {
+    it('Received value should be 3.14', function (done) {
+        var messageHandler = function (e) {
+            window.removeEventListener('message', messageHandler);
+            if (e.data != '3.14') {
+                done(new Error("Value is " + e.data + ' but expected to be 3.14'));
             } else {
-                setTimeout(function() {
-                    done();
-                });
+                done();
             }
-        });
+        };
 
+        window.addEventListener('message', messageHandler);
         porthole.sendMessage('3.14');
     });
 });
