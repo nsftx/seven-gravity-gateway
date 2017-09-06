18666b1001f888edf6709c29700ffcfab4336749
Pandzic Ivan <pandzic.ivan@nsoft.ba>
2017-09-06 14:27:07 +0200

    Fix bundling
    ref T35504

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,47 @@
 const Clean = require('clean-webpack-plugin');
 const Uglify = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-    entry: {
-        'index' : './index.js',
-        'index.min' : './index.js',
-        'master': './master.js',
-        'master.min': './master.js',
-        'slave': './slave.js',
-        'slave.min': './slave.js',
-        'plugin-storage': './plugin-storage.js',
-        'plugin-storage.min': './plugin-storage.js'
+// Webpack accepts array of configurations
+// https://github.com/webpack/webpack/issues/1189#issuecomment-227133370
+
+module.exports = [
+    {
+        entry: {
+            'index' : './index.js',
+            'master': './master.js',
+            'slave': './slave.js',
+            'plugin-storage': './plugin-storage.js',
+        },
+        plugins: [
+            new Clean(['dist']),
+        ],
+        output: {
+            filename: '[name].js',
+            path: __dirname + '/dist',
+            library: ['gravity', 'gateway', '[name]'],
+            libraryTarget: 'umd',
+            umdNamedDefine: true
+        }
     },
-    plugins: [
-        new Clean(['dist']),
-        new Uglify({
-            include: /\.min\.js$/i,
-            minimize: true
-        })
-    ],
-    output: {
-        filename: '[name].js',
-        path: __dirname + '/dist',
-        library: ['gravity', 'gateway', '[name]'],
-        libraryTarget: 'umd',
-        umdNamedDefine: true
+    {
+        entry: {
+            'index' : './index.js',
+            'master': './master.js',
+            'slave': './slave.js',
+            'plugin-storage': './plugin-storage.js'
+        },
+        plugins: [
+            new Uglify({
+                include: /\.js$/i,
+                minimize: true
+            })
+        ],
+        output: {
+            filename: '[name].min.js',
+            path: __dirname + '/dist',
+            library: ['gravity', 'gateway', '[name]'],
+            libraryTarget: 'umd',
+            umdNamedDefine: true
+        }
     }
-};
+];
