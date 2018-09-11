113e1ded9d36c301938d8facb939e6f28c33edc4
ivan.pandzic <pandzic.ivan@nsoft.com>
2018-09-11 11:33:14 +0200

    Add slave failing tests
    ref T51923

--- test/slave_gateway.js
+++ test/slave_gateway.js
@@ -1,5 +1,7 @@
 var Gateway = require('../src/slave_gateway'),
     assert = require('assert'),
+    sinon = require('sinon'),
+    expect = require('chai').expect,
     dom = require('jsdom-global')(); //Inject dom in test because window deps
 
 describe('Slave gateway instantiation', function() {
@@ -43,5 +45,72 @@ describe('Slave gateway instantiation', function() {
 
     it('Instantiation should return existing instance', function() {
         assert.strictEqual(Gateway(), slaveInstance);
+    });
+});
+
+describe('Slave gateway message exchange', function() {
+    var slavePorthole = require('../src/messaging/slave');
+
+    afterEach(function () {
+        sinon.restore();
+    });
+
+    it('Should send message', function() {
+        var instance = Gateway({
+            productId : 'Product',
+            allowedOrigins : ['http://www.nsoft.ba'],
+            data : {
+                config : {}
+            }
+        });
+        var spy = sinon.spy(slavePorthole, 'sendMessage');
+        instance.sendMessage({}, '*');
+        assert.equal(spy.called, true);
+    });
+
+    it('Should call subscribeCrossContextCallbacks', function() {
+        var instance = Gateway({
+            productId : 'Product',
+            allowedOrigins : ['http://www.nsoft.ba'],
+            data : {
+                config : {}
+            }
+        });
+        var spy = sinon.spy(instance, 'subscribeCrossContextCallbacks');
+        instance.sendMessage({
+            action : 'Widget.TestMsg',
+            callbacks : [
+                {
+                    title: 'Dummy msg',
+                    method: function() {
+                        // Code here
+                    }
+                }
+            ]
+        }, '*');
+        assert.equal(spy.called, true);
+    });
+
+    it('Should succesfully subscribe cross context callbacks', function() {
+        var instance = Gateway({
+            productId : 'Product',
+            allowedOrigins : ['http://www.nsoft.ba'],
+            data : {
+                config : {}
+            }
+        });
+        var msgData =  {
+            action : 'Widget.TestMsg',
+            callbacks : [
+                {
+                    title: 'Dummy msg',
+                    method: function() {
+                        // Code here
+                    }
+                }
+            ]
+        };
+        instance.sendMessage(msgData, '*');
+        expect(msgData.callbacks[0].cbHash).to.be.a('string');
     });
 });
