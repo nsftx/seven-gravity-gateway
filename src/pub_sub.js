64cc51e0c207450ffd9aaab7c6368421594bc8d5
Dinko <miletic.dinko@nsoft.com>
2021-05-04 12:24:15 +0200

    Async callback should return only passed data from Master instead of the  whole event object

--- src/pub_sub.js
+++ src/pub_sub.js
@@ -31,7 +31,7 @@ var pubSub = {
         var subscription = false;
         if(typeof callback === 'function') {
             subscription = this.subscribe(action, function(response) {
-                callback(response);
+                callback(response.data);
                 subscription.remove();
             });
         }
