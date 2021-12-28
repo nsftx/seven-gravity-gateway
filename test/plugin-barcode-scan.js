da1b9c0dd58ff627372d88e464b7727810df3d86
Dinko <miletic.dinko@nsoft.com>
2021-12-28 11:46:13 +0100

    Add tests for barcode plugin

---
+++ test/plugin-barcode-scan.js
@@ -0,0 +1,58 @@
+var assert = require('assert'),
+    sinon = require('sinon'),
+    Gateway = require('../src/slave_gateway'),
+    BarcodeScan = require('../plugin-barcode-scan'),
+    barcodeScanPplugin = new BarcodeScan();
+
+
+describe('Barcode scan', function() {
+    var slaveInstance;
+
+    beforeEach(function () {
+        slaveInstance = Gateway();
+    });
+
+    afterEach(function () {
+        sinon.restore();
+    });
+
+    it('on init should attach keydonw listener on document', function() {
+        var spy = sinon.spy(window.document, 'addEventListener');
+        barcodeScanPplugin.setUpOnce(slaveInstance);
+        assert.equal(spy.called, true);
+    });
+
+    it('should emit Slave.ScanFinished when scan is finished', function() {
+        var spy = sinon.spy(slaveInstance, 'emit');
+        var result = '8x2lh2g02';
+        var event = new KeyboardEvent('keydown', {'code': 'space'});
+        document.dispatchEvent(event);
+        result.split('').forEach(function(c) {
+            var event = new KeyboardEvent('keydown', { 'key': c });
+            document.dispatchEvent(event);
+        });
+      
+        assert(spy.calledWith, {
+            action: 'Slave.ScanFinished',
+            data: {
+                code: result
+            }
+        });
+    });
+
+    it('should emit Slave.ScanFinished when scan is finished without prefix', function() {
+        var spy = sinon.spy(slaveInstance, 'emit');
+        var result = '8x2lh2g02';
+        result.split('').forEach(function(c) {
+            var event = new KeyboardEvent('keydown', { 'key': c });
+            document.dispatchEvent(event);
+        });
+      
+        assert(spy.calledWith, {
+            action: 'Slave.ScanFinished',
+            data: {
+                code: result
+            }
+        });
+    });
+});
