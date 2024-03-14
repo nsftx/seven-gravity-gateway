6ec1dc9b1c70b1ba6506419a82ed2e623ff950fa
Salko Balić <salkobalic3@gmail.com>
2024-03-14 14:11:28 +0100

    store prevent keys on init in array

--- src/plugin/retail.js
+++ src/plugin/retail.js
@@ -1,9 +1,14 @@
+var preventKeysList = [];
+
 function Retail() {}
 
 Retail.prototype.setUpOnce = function() {
 };
 
-Retail.prototype.onLoad = function() {
+Retail.prototype.onLoad = function(slave, loadData) {
+    loadData.data.settings.preventKeys.forEach(function(key) {
+        preventKeysList.push(key);
+    });
 };
 
 module.exports = Retail;
