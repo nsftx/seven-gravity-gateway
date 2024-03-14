dba8c43beabcac500f0d4ab21f7dfe1ffeda79ec
Salko Balić <salkobalic3@gmail.com>
2024-03-14 14:21:13 +0100

    register keydown listener

--- src/plugin/retail.js
+++ src/plugin/retail.js
@@ -3,6 +3,8 @@ var preventKeysList = [];
 function Retail() {}
 
 Retail.prototype.setUpOnce = function() {
+    window.document.addEventListener('keydown', function() {
+    });
 };
 
 Retail.prototype.onLoad = function(slave, loadData) {
