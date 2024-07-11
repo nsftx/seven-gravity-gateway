678e409f977446b039bd833fe33410e8a40325ee
zoljann <zolj.nedim@nsoft.com>
2024-07-11 09:11:23 +0200

    Replace master/slave with parent/child

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,26 @@
 var logger = {
     debug : false, //Verbosity setting
+    igniteDebugTerminology: false,
 
     out : function(){
         //Convert to array
+        if (this.igniteDebugTerminology) {
+            if (typeof arguments[1] === 'string') {
+                arguments[1] = arguments[1]
+                    .replace('[GG]', '[IM]')
+                    .replace('Master', 'Parent')
+                    .replace('Slave', 'Child');
+            }
+            
+            if (typeof arguments[2] === 'string') {
+                arguments[2] = arguments[2]
+                    .replace('Slave', 'Child')
+                    .replace('slave', 'child')
+                    .replace('Master', 'Parent')
+                    .replace('master', 'parent');
+            }
+        }
+        
         var args = Array.prototype.slice.call(arguments);
         //First argument is notification type (critical, error, log, warn, info)
         var type = args.splice(0,1);
